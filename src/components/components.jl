a8791ea75c2f447b710f43b89905d533bf747998
Eunjong Kim <ekim7206@gmail.com>
2025-09-28 21:45:26 +0900

    additional refactoring

--- src/components/components.jl
+++ src/components/components.jl
@@ -3,15 +3,15 @@ module Components
 import DeviceLayout: AbstractCoordinateSystem, Coordinate, Meta
 using DeviceLayout
 
-export grayscale_bridge!, cross_marker!, ell_marker!
-
 include("alignment_marker.jl")
 using .AlignmentMarker
 
 include("cpw_resonator.jl")
 using .CPWResonator
 
-include("airbridge.jl")
+include("airbridge/airbridge.jl")
 using .AirBridge
+
+export grayscale_bridge!, cross_marker!, ell_marker!, cpw_resonator!
 
 end # end of module
